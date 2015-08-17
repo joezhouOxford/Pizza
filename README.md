a7e4501fde02856af2d4796739489b1f69ce3614
joezhouoxford <joezhou@spengineeringltd.com>
2015-08-17 12:35:02 +0100

    trigger CI

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ The workflow which automated with above best practice is as follow
 2. trigger integration tests (unit and e2e tests) with teamcity.
 3. after integration tests passes, deploy and change go live
 
-The teamcity url is http://137.116.198.227:1111/project.html?projectId=PizzaCI&tab=projectOverview
+The teamcity url is http://137.116.198.227:1111//viewType.html?buildTypeId=PizzaCI_PizzaBuild
 username:admin
 password:password01
 
--- app/pizza/Ingredient_test.js
+++ app/pizza/Ingredient_test.js
@@ -1,16 +1,21 @@
 'use strict';
 
-describe('myApp.pizza module', function() {
+describe('myApp.pizza module', function () {
 
-  beforeEach(module('myApp.pizza'));
+    beforeEach(module('myApp.pizza'));
 
-  describe('pizza controller', function(){
+    describe('pizza controller', function () {
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var ingredientCtrl = $controller('IngredientCtrl');
-      expect(ingredientCtrl).toBeDefined();
-    }));
+        it('should ....', inject(function ($controller) {
+            //spec body
+            var ingredientCtrl = $controller('IngredientCtrl');
+            expect(ingredientCtrl).toBeDefined();
+        }));
+        it('should ....', inject(function ($controller) {
+            //spec body
+            var ingredientCtrl = $controller('IngredientCtrl');
+            expect(ingredientCtrl).toBeDefined();
+        }));
 
-  });
+    });
 });
